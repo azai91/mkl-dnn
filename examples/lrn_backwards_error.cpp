e57da6a7e94d9fe66099f676d4d411dd0cfa8e06
Alexander Zai <alexzai@amazon.com>
2018-08-17 11:38:36 -0700

    init large numbers

--- examples/lrn_backwards_error.cpp
+++ examples/lrn_backwards_error.cpp
@@ -39,95 +39,92 @@ void simple_net()
 
     /* initializing non-zero values for out_grad */
     for (size_t i = 0; i < net_src.size(); ++i)
-      net_out_grad[i] = sinf((float)i);
+      net_out_grad[i] = (std::rand() % 100) - 50;;
 
     memory::dims lrn_src_tz = { 10, 96, 54, 54 };
-
-
-
-  /* out_grad is default and in_grad is default */
-    auto lrn_src_mem = memory(
-            { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
-              cpu_engine },
-            net_src.data());
 
     const uint32_t local_size = 5;
     const float alpha = 0.0001f;
     const float beta = 0.75f;
     const float k = 1.0f;
 
-    auto lrn_desc = lrn_forward::desc(prop_kind::forward_training, lrn_across_channels,
-                                      lrn_src_mem.get_primitive_desc().desc(),
+
+    /* out_grad is default and in_grad is default */
+      auto lrn_src_mem = memory(
+              { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
+                cpu_engine },
+              net_src.data());
+
+      auto lrn_desc = lrn_forward::desc(prop_kind::forward_training, lrn_across_channels,
+                                        lrn_src_mem.get_primitive_desc().desc(),
+                                        local_size, alpha, beta, k);
+      auto lrn_pd = lrn_forward::primitive_desc(lrn_desc, cpu_engine);
+
+      auto lrn_dst_memory = memory(lrn_pd.dst_primitive_desc());
+      auto lrn_workspace_memory = memory(lrn_pd.workspace_primitive_desc());
+      auto lrn = lrn_forward(lrn_pd, lrn_src_mem, lrn_workspace_memory,
+                             lrn_dst_memory);
+
+      auto lrn_diff_out_mem = memory(
+        { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
+          cpu_engine },
+        net_out_grad.data());
+
+      auto lrn_bwd_desc = lrn_backward::desc(
+              lrn_across_channels, lrn_pd.src_primitive_desc().desc(),
+              lrn_diff_out_mem.get_primitive_desc().desc(), local_size, alpha, beta, k);
+
+      auto lrn_bwd_pd
+              = lrn_backward::primitive_desc(lrn_bwd_desc, cpu_engine, lrn_pd);
+
+      auto lrn_diff_src_memory = memory(
+        { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
+          cpu_engine },
+        net_in_grad.data());
+
+      auto lrn_bwd
+              = lrn_backward(lrn_bwd_pd, lrn_src_mem, lrn_diff_out_mem,
+                             lrn_workspace_memory, lrn_diff_src_memory);
+
+
+    /* out_grad is default and in_grad is custom */
+    auto lrn_src_mem_custom = memory(
+        { { { lrn_src_tz }, memory::data_type::f32, memory::format::nChw8c },
+          cpu_engine },
+        net_src.data());
+
+    auto lrn_desc_custom = lrn_forward::desc(prop_kind::forward_training, lrn_across_channels,
+                                             lrn_src_mem_custom.get_primitive_desc().desc(),
                                       local_size, alpha, beta, k);
-    auto lrn_pd = lrn_forward::primitive_desc(lrn_desc, cpu_engine);
+    auto lrn_pd_custom = lrn_forward::primitive_desc(lrn_desc_custom, cpu_engine);
 
-    auto lrn_dst_memory = memory(lrn_pd.dst_primitive_desc());
-    auto lrn_workspace_memory = memory(lrn_pd.workspace_primitive_desc());
-    auto lrn = lrn_forward(lrn_pd, lrn_src_mem, lrn_workspace_memory,
-                           lrn_dst_memory);
-
-    memory::dims lrn_diff_dst_tz = { 32, 8, 54, 54};
-
-    auto lrn_diff_out_mem = memory(
-      { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
-        cpu_engine },
-      net_out_grad.data());
-
-    auto lrn_bwd_desc = lrn_backward::desc(
-            lrn_across_channels, lrn_pd.src_primitive_desc().desc(),
-            lrn_diff_out_mem.get_primitive_desc().desc(), local_size, alpha, beta, k);
-
-    auto lrn_bwd_pd
-            = lrn_backward::primitive_desc(lrn_bwd_desc, cpu_engine, lrn_pd);
-
-    auto lrn_diff_src_memory = memory(
-      { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
-        cpu_engine },
-      net_in_grad.data());
-
-    auto lrn_bwd
-            = lrn_backward(lrn_bwd_pd, lrn_src_mem, lrn_diff_out_mem,
-                           lrn_workspace_memory, lrn_diff_src_memory);
+    auto lrn_dst_memory_custom = memory(lrn_pd_custom.dst_primitive_desc());
+    auto lrn_workspace_memory_custom = memory(lrn_pd_custom.workspace_primitive_desc());
+    auto lrn_custom = lrn_forward(lrn_pd_custom, lrn_src_mem_custom, lrn_workspace_memory_custom,
+                                  lrn_dst_memory_custom);
 
 
-  /* out_grad is default and in_grad is custom */
-  auto lrn_src_mem_custom = memory(
-      { { { lrn_src_tz }, memory::data_type::f32, memory::format::nChw8c },
-        cpu_engine },
-      net_src.data());
+    auto lrn_diff_out_mem_custom = memory(
+        { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
+          cpu_engine },
+        net_out_grad.data());
 
-  auto lrn_desc_custom = lrn_forward::desc(prop_kind::forward_training, lrn_across_channels,
-                                           lrn_src_mem_custom.get_primitive_desc().desc(),
-                                    local_size, alpha, beta, k);
-  auto lrn_pd_custom = lrn_forward::primitive_desc(lrn_desc_custom, cpu_engine);
-
-  auto lrn_dst_memory_custom = memory(lrn_pd_custom.dst_primitive_desc());
-  auto lrn_workspace_memory_custom = memory(lrn_pd_custom.workspace_primitive_desc());
-  auto lrn_custom = lrn_forward(lrn_pd_custom, lrn_src_mem_custom, lrn_workspace_memory_custom,
-                                lrn_dst_memory_custom);
+    auto lrn_bwd_desc_custom = lrn_backward::desc(
+        lrn_across_channels, lrn_pd_custom.src_primitive_desc().desc(),
+        lrn_diff_out_mem_custom.get_primitive_desc().desc(), local_size, alpha, beta, k);
 
 
-  auto lrn_diff_out_mem_custom = memory(
-      { { { lrn_src_tz }, memory::data_type::f32, memory::format::nchw },
-        cpu_engine },
-      net_out_grad.data());
+    auto lrn_bwd_pd_custom
+        = lrn_backward::primitive_desc(lrn_bwd_desc_custom, cpu_engine, lrn_pd_custom);
 
-  auto lrn_bwd_desc_custom = lrn_backward::desc(
-      lrn_across_channels, lrn_pd_custom.src_primitive_desc().desc(),
-      lrn_diff_out_mem_custom.get_primitive_desc().desc(), local_size, alpha, beta, k);
+    auto lrn_diff_src_memory_custom = memory(
+        { { { lrn_src_tz }, memory::data_type::f32, memory::format::nChw8c },
+          cpu_engine },
+        net_in_grad_custom.data());
 
-
-  auto lrn_bwd_pd_custom
-      = lrn_backward::primitive_desc(lrn_bwd_desc_custom, cpu_engine, lrn_pd_custom);
-
-  auto lrn_diff_src_memory_custom = memory(
-      { { { lrn_src_tz }, memory::data_type::f32, memory::format::nChw8c },
-        cpu_engine },
-      net_in_grad_custom.data());
-
-  auto lrn_bwd_custom
-      = lrn_backward(lrn_bwd_pd_custom, lrn_src_mem_custom, lrn_diff_out_mem_custom,
-                     lrn_workspace_memory_custom, lrn_diff_src_memory_custom);
+    auto lrn_bwd_custom
+        = lrn_backward(lrn_bwd_pd_custom, lrn_src_mem_custom, lrn_diff_out_mem_custom,
+                       lrn_workspace_memory_custom, lrn_diff_src_memory_custom);
 
     std::vector<primitive> net_bwd;
     net_bwd.push_back(lrn_bwd);
